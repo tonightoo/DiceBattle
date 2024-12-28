c36baa509918991d2c3fa3682dda7bd6a906ed10
tonightoo <tonightoo.development@gmail.com>
2024-12-29 02:20:32 +0900

    add TestProject

--- DiceBattle/Program.cs
+++ DiceBattle/Program.cs
@@ -4,8 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static DxLibDLL.DX;
-using Domain;
-using System.Diagnostics.Eventing.Reader;
+using Domain.DataObject;
 
 namespace DiceBattle
 {
---
+++ TestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("30202b4e-cafe-4c5e-ad69-9a70f4b0dda5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
