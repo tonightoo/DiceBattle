c4a51a6c9eea09594fba13ff322cdb89f1c5df63
tonightoo <tonightoo.development@gmail.com>
2025-01-13 14:19:32 +0900

    add image file repository

---
+++ Application/Repositories/IImageRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.DataObjects;
+using System.Collections.Generic;
+
+namespace Application.Repositories
+{
+    public interface IImageRepository
+    {
+
+        Image Get(int imageId);
+
+        void Add(int imageId, Image image);
+
+        Dictionary<int, Image> GetAll();
+
+    }
+}
---
+++ InMemoryInfra/InMemoryImageRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Repositories;
+using Domain.DataObjects;
+
+namespace InMemoryInfra
+{
+    public class InMemoryImageRepository : IImageRepository
+    {
+
+        public Dictionary<int, Image> Images = new Dictionary<int, Image>();
+
+        public Image Get(int imageId)
+        {
+            return Images[imageId];
+        }
+
+        public void Add(int imageId, Image image)
+        {
+            if (Images.ContainsKey(imageId))
+            {
+                Images[imageId] = image;
+            }
+            else
+            {
+                Images.Add(imageId, image);
+            }
+        }
+
+        public Dictionary<int, Image> GetAll()
+        {
+            return Images;
+        }
+
+    }
+}
